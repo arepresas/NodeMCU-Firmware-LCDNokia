80f8338e1df85f5677ae8518de55f852298765ec
Overlag <abraham.represas@gmail.com>
2017-01-15 16:56:02 +0100

    Change Build date and version

--- app/include/user_version.h
+++ app/include/user_version.h
@@ -6,9 +6,9 @@
 #define NODE_VERSION_REVISION	4U
 #define NODE_VERSION_INTERNAL   1U
 
-#define NODE_VERSION	"NodeMCU 1.5.4.1"
+#define NODE_VERSION	"NodeMCU 1.5.4.1 NokiaLCD Mod"
 #ifndef BUILD_DATE
-#define BUILD_DATE	  "unspecified"
+#define BUILD_DATE	  "15/01/2017"
 #endif
 
 extern char SDK_VERSION[];
